d06cce4b38d169286db790a324a90b05dcfa6a1f
Babatunde Okediran <babatunde.okediran@katsupay.com>
2026-08-10 10:25:50 +0100

    Init push

---
+++ src/main/java/com/xodium/useraccountservice/UseraccountserviceApplication.java
@@ -0,0 +1,13 @@
+package com.xodium.useraccountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UseraccountserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UseraccountserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xodium/useraccountservice/UseraccountserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xodium.useraccountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UseraccountserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
